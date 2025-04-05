60bcba7bfa1fceeeaf6c1a87be9e6838dd221923
Jaspak1778 <pekkala.jani@hotmail.fi>
2025-04-05 20:20:14 +0300

    GPG toimii nyt?

---
+++ Python1PV.py
@@ -0,0 +1,31 @@
+import random
+print("Arvaa luku peli, sinulla on kolme arvaus kertaa ! ")
+#Alustetaan tarvittavat muuttujat
+#Uusikomment
+#Uusikomment
+#Alustetaan tarvittavat muuttujat
+#toimiiko gpg
+
+
+randomi = random.randint(1, 20)
+laskuri = 3
+while True:
+    vastaus = (input("Arvaa luku: "))
+    try:
+        vastaus = int(vastaus)
+    except ValueError:
+        print(vastaus , "ei ole kokonaisluku, Anna luku 1 ja 20 väliltä!")
+        continue
+    if randomi == vastaus:
+        print("oikein")
+        break
+    else:
+
+        print("Arvasit väärin")
+        laskuri -= 1
+        if laskuri > 0:
+            print(f'Arvauksia jäljellä {laskuri}')
+        else:
+            print(f'Oikea vastaus olisi ollut {randomi} ..')
+            break
+
